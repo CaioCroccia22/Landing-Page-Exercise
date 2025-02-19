7b1deb5b90d0abd55807266a9d2b6f22ba83afc2
caiocroccia22 <caiocroccia22@gmail.com>
2025-02-18 22:04:17 -0300

    feat: implement header without background/with news links , and fix background body position

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,17 @@ const sass = require('gulp-sass') (require('sass'));
 function buildStyles(){
     return gulp.src('src/styles/*.scss')
         // Compila o Sass para CSS
-        .pipe(sass({output: 'compressed'}))
+        .pipe(sass({outputStyle: 'compressed'}))
         .pipe(gulp.dest('dist/styles'))
 }
 
 //Redução de espaço de imagem
-function reduceImage(){
-    return gulp.src('./src/img/*')
+function reduceImage() {
+    return gulp.src('src/img/**/*.{png,jpg,jpeg,gif,svg}') // Busca todas as imagens
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'))
+        .pipe(gulp.dest('dist/img')) // Copia para dist/img
+        .on('end', () => console.log('Imagens copiadas corretamente!'));
 }
-
-
-
 // Observação para desenvolvimento -> Comando definido no pack.json: npm run dev
 function watchFiles(){
     gulp.watch('src/styles/*.scss', buildStyles)
@@ -29,4 +27,5 @@ function watchFiles(){
 
 module.exports.watch = watchFiles;
 module.exports.buildStyles = buildStyles;
+module.exports.reduceImage = reduceImage;
 module.exports.default = parallel(watchFiles, buildStyles, reduceImage);
